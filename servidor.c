81c393e435bfb33cb948df5c3b9a487dbf4767c9
xXMario257Xx <32972055+xXMario257Xx@users.noreply.github.com>
2021-08-18 14:30:26 -0500

    Add files via upload

--- servidor.c
+++ servidor.c
@@ -19,9 +19,10 @@
 #define MAXLINE 4096
 #define TRUE 1
 char line[MAXLINE];
-int alt[4]; char numm[4];
+int alt[4], clientes[9], ganador; char numm[4];
+bool bandera = false;
 
-servicio( int sock);
+
 
 int crearsocket( int *port, int type){
 	int sockfd;
@@ -68,7 +69,6 @@ bool compareTo(char v1, char v2){
 	bool igual = false;
 	if (v1 == v2){
 		igual = true;
-		//printf("ERROR. Numero con valores repetidos, digite otro \n");
 	}
 	return igual;
 }
@@ -101,13 +101,12 @@ bool esEntero(char arr[], int t){
 		if(validar_rpt(arr))
 			eSentero = false;
 	} else {
-		//printf("ERROR. Digite un numero de 4 digitos\n");
 		eSentero = false;
 	} 
 	return eSentero;
 }
 
-generar_alt(){
+void generar_alt(){
 	srand(time(NULL));
 	do{
 		for (int i=0; i < 4; i++){
@@ -116,6 +115,53 @@ generar_alt(){
 		}
 	}while(validar_rpt(numm));
 }
+
+void servicio( int sock){
+	int n ;
+	for (;;) {
+		n = read( sock, line, MAXLINE );
+		if(n <= 0){
+			return;
+		}
+		printf("Mensaje : %s", line);
+		if (bandera == false){
+			if (esEntero(line, n-1)){
+				int c = 0;
+				for (int i = 0; i<4; i++){
+					if (compareTo(numm[i], line[i]))
+						c++;
+				}
+				int c2 = 4-c;
+				char value1 = c + '0', value2 = c2 + '0';
+				if (c == 4){
+					bandera = true;
+					char congr[] = "FELICITACIONES\n";
+					int tr = sizeof(congr);
+					write(sock, congr, tr);
+					exit(0);
+				} else {
+					char cadena[] = {value1,' ','F','I','J','A','S','\n',value2,' ','P','I','C','A','S','\n','\0'};
+					int m = sizeof(cadena);
+					write(sock, cadena, m);
+				}
+			} else {
+				char cadena[] = "ERROR, Digite un numero de 4 digitos\n";
+				int m = sizeof(cadena);
+				write(sock, cadena, m);
+			}
+		} else {
+			char congr[] = "Lo siento, numero ya encontrado\n";
+			int ct = sizeof(congr);
+			write(sock, congr, ct);
+			if (fork() == 0){
+				exit(-1);
+			}
+		}
+		printf("---------------------------------------------------------\n");
+		memset(line, 0, MAXLINE); 
+	}
+}
+
 
 int main( int argc, char*argv[]){
 
@@ -129,7 +175,7 @@ int main( int argc, char*argv[]){
 		exit(1);
 	}
 
-	port = atoi( argv[1]);
+	port = atoi(argv[1]);
 
 	if(( sock_escucha = crearsocket( &port, SOCK_STREAM ))== -1){
 		fprintf( stderr, "Error: No se pudo crear/conectar socket\n");
@@ -138,17 +184,19 @@ int main( int argc, char*argv[]){
 
 	signal( SIGCHLD, sigchld);
 	listen( sock_escucha, 1024);
+	
 	fprintf( stdout, "Inicio servidor en el puerto %d\n", port);
 	generar_alt();
-	int num = (alt[0]*1000) + (alt[1]*100) + (alt[2]*10) + alt[3];
-	printf("NUMERO ALEATORIO GENERADO :  %i \n----------------------------\n", num);
-
+	printf("NUMERO ALEATORIO GENERADO :  %i%i%i%i \n----------------------------\n", alt[0], alt[1], alt[2], alt[3]);
+	
+	
 	while(TRUE){
 
 		lgadr = sizeof(adr);
 		sock_servicio = accept(sock_escucha,(struct sockaddr*)&adr,&lgadr);
-		fprintf( stdout, "Servicio aceptado\n");
-
+		
+		printf("Servicio aceptado \n");
+		
 		if(fork() == 0){
 			close(sock_escucha);
 			servicio( sock_servicio );
@@ -159,45 +207,4 @@ int main( int argc, char*argv[]){
 }
 
 
-
-
-
-servicio( int sock){
-	int n ;
-	for (;;) {
-		n = read( sock, line, MAXLINE );
-		if(n <= 0){
-			return;
-		}
-		printf("Mensaje : %s", line);
-		bool v = esEntero(line, n-1);
-		if (v){
-			int c = 0;
-			for (int i = 0; i<4; i++){
-				char value = alt[i] + '0';
-				if (compareTo(value, line[i]))
-					c++;
-			}
-			int c2 = 4-c;
-			char value1 = c + '0';
-			char value2 = c2 + '0';
-			if (c == 4){
-				char congr[] = "FELICITACIONES, NO GANASTE NADA PERO IGUAL FELICITACIONES";
-				int tr = sizeof(congr);
-				write(sock, congr, tr);
-			} else {
-				char cadena[] = {value1,' ','F','I','J','A','S','\n',value2,' ','P','I','C','A','S','\n','\0'};
-				int m = sizeof(cadena);
-				write(sock, cadena, m);
-			}
-		} else {
-			char cadena[] = "ERROR,Digite un numero de 4 digitos\n";
-			int m = sizeof(cadena);
-			write(sock, cadena, m);
-		}
-		printf("---------------------------------------------------------\n");
-		//write(sock, line, n);
-		memset(line, 0, MAXLINE); 
-	}
-}
 
